5fd9b9f1a6f5fe8edaf434a4744be208fd229e29
Nwakanma Emmanuel <nwakanmaemmanuel60@gmail.com>
2026-08-31 01:08:28 +0100

    Refactor database connection to use Pool instead of Client and update error logging

--- src/config/db.js
+++ src/config/db.js
@@ -1,23 +1,24 @@
 import pg from "pg";
 import dotenv from "dotenv";
-dotenv.config();
-const { Client } = pg;
 
-const client = new Client({
-  host: process.env.DB_HOST ,
+dotenv.config();
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
 });
 
-
-client.connect((err) => {
-  if (err) {
-    console.error("Error connecting to the database:", err);
-  } else {
-    console.log("Connected to the database");
-  }
+pool.on("connect", () => {
+  console.log("Connected to the database");
 });
 
-export default client;
+pool.on("error", (err) => {
+  console.error("Unexpected database error:", err);
+});
+
+export default pool;
